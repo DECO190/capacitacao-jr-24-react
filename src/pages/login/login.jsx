560596b830be00af413ae219a7b69b8e1f3b4894
DECO190 <deco9871@gmail.com>
2024-07-25 19:47:36 -0300

    dev

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,16 +1,33 @@
 import { useState } from "react"
 import "./login.css"
+import { userLoginApi } from "./api/user-login-api"
+import { useNavigate } from "react-router-dom"
 
 export function Login() {
     let [email, setEmail] = useState("")
     let [password, setPassword] = useState("")
+    let navigate = useNavigate()
+
+    const userLogin = async () => {
+        if (!email || !password) return
+
+        let {data, response} = await userLoginApi(email, password)
+
+        if (response.ok) {
+            if (data.is_employee) {
+                navigate("/funcionario")
+            } else {
+                navigate("/cliente")
+            }
+        }
+    }
 
     return (
         <div className="login-main-container">
             <input type="text" placeholder="Email" onChange={(ev) => setEmail(ev.target.value)}/>
             <input type="text" placeholder="Senha" onChange={(ev) => setPassword(ev.target.value)}/>
 
-            <button>Entrar</button>
+            <button onClick={() => userLogin()}>Entrar</button>
         </div>
     )
 }
