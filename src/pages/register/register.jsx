433502c9b08fcdf5e771ed8541ac77a9334e7992
DECO190 <deco9871@gmail.com>
2024-07-25 20:29:16 -0300

    dev

--- src/pages/register/register.jsx
+++ src/pages/register/register.jsx
@@ -3,6 +3,7 @@ import dogImage from "../../assets/dog.png"
 import { useState } from "react"
 import { registerUserApi } from "./api/register-user-api"
 import { useNavigate } from "react-router-dom"
+import { getCEPInfosApi } from "./api/get-cep-infos-api"
 
 export function Register() {
     let [registerInfos, setRegisterInfos] = useState({
@@ -11,8 +12,20 @@ export function Register() {
         password: ""
     })
 
+    let [CEP, setCEP] = useState("")
+    let [CEPInfos, setCEPInfos] = useState({})
+
     let [repeatPassword, setRepeatPassword] = useState("")
     let navigate = useNavigate()
+
+    const getCEPInfos = async () => {
+        console.log("dsadsa")
+        if (!CEP) return
+
+        let {response, data} = await getCEPInfosApi(CEP)
+        console.log(data)
+        setCEPInfos(data)
+    }
 
     const registerUser = async () => {
         for (let k in registerInfos) {
@@ -28,7 +41,6 @@ export function Register() {
         let {response, data} = await registerUserApi(registerInfos)
 
         if (response.ok) {
-            console.log("USER CADASTRADO COM SUCESSO!")
             navigate("/users")
         }
     }
@@ -38,12 +50,18 @@ export function Register() {
             <img src={dogImage} alt="" />
 
             <p>
-                {JSON.stringify(registerInfos, null, 2)}
+                {JSON.stringify(CEPInfos, null, 2)}
             </p>
 
             <input placeholder="Nome" onChange={(ev) => setRegisterInfos(prev => ({...prev, name: ev.target.value}))} />
             <input placeholder="Email" onChange={(ev) => setRegisterInfos(prev => ({...prev, email: ev.target.value}))} />
             <input placeholder="Senha" onChange={(ev) => setRegisterInfos(prev => ({...prev, password: ev.target.value}))} />
+            <input placeholder="CEP" onBlur={(ev) => {
+                getCEPInfos()
+            }} onChange={(ev) => {
+                setCEP(ev.currentTarget.value)
+            }}/>
+            <input value={CEPInfos.bairro} placeholder="Bairro"/>
             <input placeholder="Repetir senha" onChange={(ev) => setRepeatPassword(ev.target.value)} />
 
             <button onClick={() => registerUser()}>Registrar</button>
