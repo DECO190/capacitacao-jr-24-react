5b3a305f9f424474e4b95e718e8b39ed6a8a1149
DECO190 <deco9871@gmail.com>
2024-07-25 17:31:30 -0300

    feat: register page

--- src/pages/register/register.jsx
+++ src/pages/register/register.jsx
@@ -1,6 +1,8 @@
 import "./register.css"
 import dogImage from "../../assets/dog.png"
 import { useState } from "react"
+import { registerUserApi } from "./api/register-user-api"
+import { useNavigate } from "react-router-dom"
 
 export function Register() {
     let [registerInfos, setRegisterInfos] = useState({
@@ -10,6 +12,26 @@ export function Register() {
     })
 
     let [repeatPassword, setRepeatPassword] = useState("")
+    let navigate = useNavigate()
+
+    const registerUser = async () => {
+        for (let k in registerInfos) {
+            if (!registerInfos[k]) {
+                return
+            }
+        }
+
+        if (registerInfos.password != repeatPassword) {
+            return 
+        }
+
+        let {response, data} = await registerUserApi(registerInfos)
+
+        if (response.ok) {
+            console.log("USER CADASTRADO COM SUCESSO!")
+            navigate("/users")
+        }
+    }
 
     return (
         <div className="register-main-container">
@@ -19,17 +41,12 @@ export function Register() {
                 {JSON.stringify(registerInfos, null, 2)}
             </p>
 
-            <input placeholder="Nome" onChange={(ev) => {
-                setRegisterInfos(prev => {
-                    return {...prev, name: ev.target.value}
-                })
-            }}/>
-            <input placeholder="Email" />
-            <input placeholder="Senha" />
-            <input placeholder="Repetir senha" />
+            <input placeholder="Nome" onChange={(ev) => setRegisterInfos(prev => ({...prev, name: ev.target.value}))} />
+            <input placeholder="Email" onChange={(ev) => setRegisterInfos(prev => ({...prev, email: ev.target.value}))} />
+            <input placeholder="Senha" onChange={(ev) => setRegisterInfos(prev => ({...prev, password: ev.target.value}))} />
+            <input placeholder="Repetir senha" onChange={(ev) => setRepeatPassword(ev.target.value)} />
 
-            <button>Registrar</button>
-
+            <button onClick={() => registerUser()}>Registrar</button>
         </div>
     )
 }
